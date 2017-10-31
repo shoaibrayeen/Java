6316f51183f55bc21bf681998979fbd4a4147a97
MOHD SHOAIB RAYEEN <31645414+shoaibrayeen@users.noreply.github.com>
2017-10-31 21:53:38 +0530

    Toggle Nth Bit of An Integer Number

---
+++ toogleNthBit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class tog
+{
+	public static void main(String[] args)
+	{
+		Scanner obj=new Scanner(System.in);
+		int number,bit;
+		System.out.print("\nEnter the Number\t:\t");
+		number=obj.nextInt();
+		System.out.print("\nEnter the bit you want to toggle\t:\t");
+		bit=obj.nextInt();
+		number= number^(1<<(bit-1));
+		System.out.print("\nNumber after toggling the " + bit + "th bit\t:\t"+number);
+		System.out.println();
+	}	
+}
+	
